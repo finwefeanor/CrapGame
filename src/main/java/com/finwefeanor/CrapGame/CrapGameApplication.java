54751167e2ccb1756c5e2ab1d487374846b1b4e1
UMUT YILDIRIM <yolgezerestel@gmail.com>
2023-10-11 15:35:02 +0300

    Initial commit

---
+++ src/main/java/com/finwefeanor/CrapGame/CrapGameApplication.java
@@ -0,0 +1,13 @@
+package com.finwefeanor.CrapGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrapGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrapGameApplication.class, args);
+	}
+
+}
